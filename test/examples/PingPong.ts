0348bbd7e37d99614a719c3684ca5145af023dd9
g.iwerutee <g.iwerutee@gmail.com>
2022-09-26 19:40:00 +0100

    Add test showing that FixedWindow can not enforce average rate

--- test/examples/PingPong.ts
+++ test/examples/PingPong.ts
@@ -4,6 +4,8 @@ import { ethers } from "hardhat";
 import { PingPong, PingPong__factory } from "../../typechain-types";
 
 const ONE_HOUR = 60 * 60;
+const WINDOW = 60;
+const MAX_HITS = 10;
 
 describe("PingPong", function () {
     let owner: SignerWithAddress;
@@ -26,18 +28,18 @@ describe("PingPong", function () {
 
     });
 
-    it("should return a pong to our ping", async function () {
+    it("should call the ping() function and return the expected result", async function () {
         expect(await pingPong.callStatic.ping()).to.equal("pong");
     });
 
-    it("should allow calls up until the rate limit", async function () {
-        for (let i = 0; i < 10; i++) {
+    it("should allow calls to ping() up until the rate limit", async function () {
+        for (let i = 0; i < MAX_HITS; i++) {
             await (await pingPong.ping()).wait();
         }
     });
 
-    it("should revert calls exceeding the rate limit", async function () {
-        for (let i = 0; i < 10; i++) {
+    it("should revert calls to ping() exceeding the rate limit", async function () {
+        for (let i = 0; i < MAX_HITS; i++) {
             await (await pingPong.ping()).wait();
         }
         
@@ -45,7 +47,7 @@ describe("PingPong", function () {
     });
 
     it("should reset the rate limit when enough time has passed", async function () {
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < MAX_HITS; i++) {
             await (await pingPong.ping()).wait();
         }
         
@@ -53,6 +55,37 @@ describe("PingPong", function () {
 
         await ethers.provider.send("evm_increaseTime", [ONE_HOUR]);
 
-        await (await pingPong.ping()).wait();
+        for (let i = 0; i < MAX_HITS; i++) {
+            await (await pingPong.ping()).wait();
+        }
+
+    });
+
+    it("should not enforce an average rate", async function () {
+        // we want to arrive in the final 10% of the current window
+        const secondsToForward = WINDOW - ((Date.now() / 1000) % WINDOW) + (WINDOW * 0.9);
+
+        // then wait until the next window has started
+        const secondsToWait = (WINDOW * 0.2);
+
+        await ethers.provider.send("evm_increaseTime", [Math.round(secondsToForward)]);
+
+        // use up our rate limit for the current window
+        for (let i = 0; i < MAX_HITS; i++) {
+            await (await pingPong.ping()).wait();
+        }
+        
+        // mine a block so that block.timestamp is increased
+        await ethers.provider.send("evm_mine", []);
+        // wait until the next window has begun
+        await ethers.provider.send("evm_increaseTime", [Math.round(secondsToWait)]);
+
+        // use up our rate limit again
+        // at this point, we've hit 2x the rate limit in (0.3 * WINDOW) continuous time
+        // which is a limitation of this rate limiting method
+        for (let i = 0; i < MAX_HITS; i++) {
+            await (await pingPong.ping()).wait();
+        }
+
     });
 });
